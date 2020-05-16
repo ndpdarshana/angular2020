86da08da5d72a302da9b27889594e90c74d1030f
Prabhath Darshana <ndp.mymail@gmail.com>
2020-05-16 15:26:37 +1000

    adding custom component

--- apps/angularapp/src/app/app.module.ts
+++ apps/angularapp/src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
     AppComponent, 
-    LoginComponent
+    LoginComponent, 
+    ServerComponent
   ],
   imports: [
     BrowserModule, 
-    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
