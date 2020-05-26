4d0dcb67e0f039c1f618906403a61ad3767b9fda
Prabhath Darshana <ndp.mymail@gmail.com>
2020-05-26 13:49:42 +1000

    Recipe list

--- apps/angularapp/src/app/recipes/recipe-list/recipe-list.component.ts
+++ apps/angularapp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model'; 
 
 @Component({
   selector: 'angularapp-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is an recipe', 
+    'https://www.gimmesomeoven.com/wp-content/uploads/2014/03/Cajun-Jambalaya-Recipe-with-Andouille-Sausage-Shrimp-and-Chicken-32.jpg')
+  ];
 
   constructor() { }
 
